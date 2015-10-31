f7277c98dbed888e06653e0cff802cf79c244092
Magdiel Juárez Guerrero <magdiel@MacBook-Pro-de-Magdiel.local>
2015-10-31 04:45:14 -0600

    Pruebas de ventana emergente con éxito

--- TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/MainActivity.java
+++ TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.magdiel.taxiseguro;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button pedir = (Button)findViewById( R.id.pedir );
+
+        pedir.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        Intent abrir = new Intent(this, pedirTaxi.class);
+        startActivity(abrir);
     }
 }
