f4e793d00f77d98fe581b2b3cb2ff00cba3c8e58
Magdiel Juárez Guerrero <magdiel@MacBook-Pro-de-Magdiel.local>
2015-10-31 04:27:45 -0600

    Pruebas de ventana emergente

--- TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/pedirTaxi.java
+++ TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/pedirTaxi.java
@@ -1,13 +1,55 @@
 package com.example.magdiel.taxiseguro;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class pedirTaxi extends AppCompatActivity {
+
+    ImageButton b1;
+    ImageButton b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pedir_taxi);
+
+        //Capturamos el id de los botones sobre los que queremos realizar la acción de mostrar un elemento emergente
+        b1 = (ImageButton)findViewById( R.id.locate );
+        b2 = (ImageButton)findViewById( R.id.taxi );
+
+        b1.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View view) {
+                        popupMenu();
+                    }});
+    }
+
+    private void popupMenu() {
+        //Crea instancia a PopupMenu
+        PopupMenu popup = new PopupMenu(this, b1);
+        popup.getMenuInflater().inflate(R.menu.datos_origen, popup.getMenu());
+
+        //registra los eventos click para cada item del menu
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.action_one) {
+                    Toast.makeText(pedirTaxi.this,
+                            "Ejecutar : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.action_two) {
+                    Toast.makeText(pedirTaxi.this,
+                            "Ejecutar : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.action_three) {
+                    Toast.makeText(pedirTaxi.this,
+                            "Ejecutar : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+        popup.show();
     }
 }
