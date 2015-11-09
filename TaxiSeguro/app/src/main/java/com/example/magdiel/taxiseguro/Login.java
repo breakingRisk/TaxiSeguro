5a60aa9f559ae24d5aa020ada4ec3fade4e63c7f
Magdiel Juárez Guerrero <magdiel@MacBook-Pro-de-Magdiel.local>
2015-11-09 15:17:25 -0600

    Probando con JSON

--- TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/Login.java
+++ TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/Login.java
@@ -1,11 +1,24 @@
 package com.example.magdiel.taxiseguro;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Login extends AppCompatActivity implements View.OnClickListener{
     String IP="169.254.234.218";
@@ -14,6 +27,14 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
     String contrasena;
     EditText user;
     EditText pass;
+    private ProgressDialog pDialog;
+
+    // Clase JSONParser
+    JSONParser jsonParser = new JSONParser();
+
+    //ids
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +51,91 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
     }
 
     public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.login:
+                new AttemptLogin().execute();
+                break;
+
+        }
+    }
+
+    class AttemptLogin extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(Login.this);
+            pDialog.setMessage("Attempting login...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            int success;
+            String username = user.getText().toString();
+            String password = pass.getText().toString();
+            try {
+                // Building Parameters
+                List params = new ArrayList();
+                params.add(new BasicNameValuePair("username", username));
+                params.add(new BasicNameValuePair("password", password));
+
+                Log.d("request!", "starting");
+                // getting product details by making HTTP request
+                JSONObject json = jsonParser.makeHttpRequest(SERVER, "POST",
+                        params);
+
+                // check your log for json response
+                Log.d("Login attempt", json.toString());
+
+                // json success tag
+                success = json.getInt(TAG_SUCCESS);
+                if (success == 1) {
+                    Log.d("Login Successful!", json.toString());
+                    // save user data
+                    SharedPreferences sp = PreferenceManager
+                            .getDefaultSharedPreferences(Login.this);
+                    SharedPreferences.Editor edit = sp.edit();
+                    edit.putString("username", username);
+                    edit.commit();
+
+                    Intent i = new Intent(Login.this, Home.class);
+                    finish();
+                    startActivity(i);
+                    return json.getString(TAG_MESSAGE);
+                } else {
+                    Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+                    return json.getString(TAG_MESSAGE);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once product deleted
+            pDialog.dismiss();
+            if (file_url != null) {
+                Toast.makeText(Login.this, file_url, Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+
+
+
+
         //Obtenemos datos de pantalla
+        /*
         nombre = user.getText().toString();
+         /*
         contrasena = pass.getText().toString();
 
         httpHandler handler = new  httpHandler();
@@ -41,6 +145,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
         TextView t2 = (TextView) findViewById(R.id.url_server);
 
         t.setText(txt);
+        */
         //t1.setText(contrasena);
         //t2.setText(SERVER);
     }
