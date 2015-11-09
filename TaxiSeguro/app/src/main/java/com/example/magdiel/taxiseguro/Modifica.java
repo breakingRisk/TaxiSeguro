4c88f4af3f4fed275f2f4a2589d82b2355d49e7b
Jess J <catostylus05@gmail.com>
2015-11-09 16:39:02 -0600

    Responsivo y Botón
    Hice algunas actividades "responsivas" y agregue el botón que nos lleva
    hacia la modificación de la contraseña

--- TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/Modifica.java
+++ TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/Modifica.java
@@ -1,13 +1,23 @@
 package com.example.magdiel.taxiseguro;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Modifica extends Activity {
+public class Modifica extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modifica);
+
+        Button b_modifica_contraseñia = (Button) findViewById(R.id.cambia_password);
+        b_modifica_contraseñia.setOnClickListener(this);
+    }
+    public void onClick(View view) {
+        Intent abrir2 = new Intent(this, Password.class);
+        startActivity(abrir2);
     }
 }
