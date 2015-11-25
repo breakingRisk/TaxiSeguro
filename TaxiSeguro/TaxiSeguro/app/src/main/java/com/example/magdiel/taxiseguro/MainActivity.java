b16a65d24002dc4ed61fb5c9b577e95102c0f9c1
xhanzary9 <xhanzary@ciencias.unam.mx>
2015-11-25 14:51:12 -0600

    se añadieron los colores a login,registro, tambien se añadieron las transiciones de los botones.

--- TaxiSeguro/TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/MainActivity.java
+++ TaxiSeguro/TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.magdiel.taxiseguro;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,19 +11,38 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 
-public class MainActivity extends Activity {
-ImageView  logota;
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    ImageView  logota;
+    Animation movimiento;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        logota = (ImageView)findViewById(R.id.logota);
-    }
-    protected void animando(){
-        Animation movimiento;
-        movimiento = AnimationUtils.loadAnimation(this,R.animator.mover);
-        movimiento.reset();
-        logota.startAnimation(movimiento);
 
+        Button b_inicio = (Button) findViewById(R.id.inicio);
+        b_inicio.setOnClickListener(this);
+
+
+        Button b_registro = (Button) findViewById(R.id.button4);
+        b_registro.setOnClickListener(this);
     }
+
+        public void onClick(View view){
+            switch (view.getId()) {
+                case R.id.inicio:
+                    Intent inicioa = new Intent(this, Login.class);
+                    startActivity(inicioa);
+                    break;
+                case R.id.button4:
+                    Intent iniciob = new Intent(this, Registro.class);
+                    startActivity(iniciob);
+                    break;
+            }
+
+
+        }
+
+
 }
--- TaxiSeguro/TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/Registro2.java
+++ TaxiSeguro/TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/Registro2.java
@@ -1,14 +1,27 @@
 package com.example.magdiel.taxiseguro;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Registro2 extends Activity {
+public class Registro2 extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro2);
+
+        TextView inc = (TextView) findViewById(R.id.linklog);
+        inc.setOnClickListener(this);
+    }
+
+    public void onClick(View view){
+        Intent inicio = new Intent(this, Login.class);
+        startActivity(inicio);
+
     }
 }
