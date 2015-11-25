f2821ca4c1a7f655d1346b5b6fa600cc54162be5
Magdiel Juárez Guerrero <magdiel@MacBook-Pro-de-Magdiel.local>
2015-11-25 03:57:54 -0600

    Modificaron colores de la interfaz pedir taxi

--- TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/pedirTaxi.java
+++ TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/pedirTaxi.java
@@ -1,13 +1,16 @@
 package com.example.magdiel.taxiseguro;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class pedirTaxi extends Activity implements View.OnClickListener {
+public class pedirTaxi extends ActionBarActivity implements View.OnClickListener {
 
     ImageButton b1;
     ImageButton b2;
@@ -21,12 +24,41 @@ public class pedirTaxi extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pedir_taxi);
 
+        setToolbar();// Añadir la Toolbar
+
         pop = (ImageButton)findViewById(R.id.locatePop);
         pop1 = (ImageButton)findViewById(R.id.DestinoPop);
         registro = (TextView)findViewById(R.id.ubicarme);
         pop.setOnClickListener(this);
         pop1.setOnClickListener(this);
         registro.setOnClickListener(this);
+    }
+
+    private void setToolbar() {
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_top, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_settings:
+                Intent i = new Intent(this, Home.class);
+                startActivity(i);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
 
     }
 
@@ -52,12 +84,9 @@ public class pedirTaxi extends Activity implements View.OnClickListener {
                 }
                 break;
             case R.id.ubicarme:
-                Toast.makeText(this, "Espere...", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Por el momento no está implementado, espere una actualización", Toast.LENGTH_SHORT).show();
                 break;
 
         }
-
-
-
     }
 }
