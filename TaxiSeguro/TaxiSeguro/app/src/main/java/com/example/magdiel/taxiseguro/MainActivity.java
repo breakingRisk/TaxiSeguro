d8679e93f9c2ee908d24d80832374df4acbcde7f
xhanzary9 <xhanzary@ciencias.unam.mx>
2015-11-25 03:18:59 -0600

    se hicieron cambios en la patalla de inicio, se vieron los colores y el logo

--- TaxiSeguro/TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/MainActivity.java
+++ TaxiSeguro/TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.magdiel.taxiseguro;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class MainActivity extends Activity {
-
+ImageView  logota;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logota = (ImageView)findViewById(R.id.logota);
+    }
+    protected void animando(){
+        Animation movimiento;
+        movimiento = AnimationUtils.loadAnimation(this,R.animator.mover);
+        movimiento.reset();
+        logota.startAnimation(movimiento);
+
     }
 }
