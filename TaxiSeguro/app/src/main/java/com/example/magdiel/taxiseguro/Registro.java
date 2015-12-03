1a0b039b0cb104739adc3c137c115a23d4dc0d02
xhazary9 <xhanzary@ciencias.unam.mx>
2015-12-02 23:36:03 -0600

    se hicieron las validaciones

--- TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/Registro.java
+++ TaxiSeguro/app/src/main/java/com/example/magdiel/taxiseguro/Registro.java
@@ -4,10 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class Registro extends Activity implements View.OnClickListener{
+public class Registro extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +21,23 @@ public class Registro extends Activity implements View.OnClickListener{
 
         Button reg = (Button) findViewById(R.id.registro);
         reg.setOnClickListener(this);
+
+        TextView inc = (TextView) findViewById(R.id.linklog);
+        inc.setOnClickListener(this);
     }
 
-    public void onClick(View view){
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.registro:
                 Intent inicioa = new Intent(this, Registro2.class);
                 startActivity(inicioa);
-
+                break;
+            case R.id.linklog:
+                Intent inicio = new Intent(this, Login.class);
+                startActivity(inicio);
+                break;
+        }
     }
 }
+
+
